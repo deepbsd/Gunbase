906da33b73e624412138e973877d7e426ee13c61
David S. Jackson <deepbsd@yahoo.com>
2017-06-20 09:56:13 -0400

    starting to display data on html page--promise is working

--- src/js/main.js
+++ src/js/main.js
@@ -5,6 +5,7 @@ const state = {
   guns: []
 }
 
+var template = '';
 const rootURL = 'http://localhost:8080/guns';
 
 //#########################################################
@@ -20,38 +21,70 @@ function getAllGuns() {
         "accept": "application/json;odata=verbose",
       },
       success: function(gundata) {
+        var num=0;
         gundata.forEach( gun => {
-
-          state.guns[gun.fullName] = gun;
+          //state.guns[gun.fullName] = gun;
+          state.guns[num] = gun;
+          num++;
         });
-        console.log('StateObject: ',state.guns);
+        //console.log('StateObject: ',state.guns);
+        console.log('first promise hit');
+        res();
       },
       error: function(error){
         console.log('error: ',error);
       }
     })
   })
+  return state;
 }
 
-getAllGuns();
+function outputGunsReport() {
+  // return new Promise( (res, rej) => {
+    template = '<div>'
+    state.guns.forEach( gun => {
+      console.log('THREE: ', typeof gun)
+      template += '<p>'+gun.fullName+'</p>'
+    })
+    template += '</div>'
+    console.log('2nd promise--template: ', template);
+    $("#output").html(template);
+  // });
+}
 
+// getAllGuns()
+// .then(function () {
+//   template = '<div>'
+//   state.guns.forEach( gun => {
+//     console.log('THREE: ', typeof gun)
+//     template += '<p>'+gun.fullName+'</p>'
+//   })
+//   template += '</div>'
+//   console.log('template: ', template);
+//   return outputGuns(template);
+// })
+// .then(function(res) {
+//     //$("#output").html(res);
+//     outputGuns(res);
+// })
+//
+// console.log('ONE:  State object: ', state.guns)
 
+getAllGuns()
+.then(outputGunsReport);
 
 //#########################################################
 //#################  DOM MODIFICATION METHODS  ############
 //#########################################################
 
-function outputGuns(){
-  var template = '<div>';
-  state.guns.forEach(gun => {
-    template += `<p>${gun.fullName}</p>`
-  })
-  return template + '</div>'
-}
+// function outputGuns(template){
+//   $("#output").html(template);
+// }
 
-var content = outputGuns();
+//var content = outputGuns();
+//console.log('Content: ', template)
 
-$('.output').html(content);
+// $("#output").html(template);
 
 
 
