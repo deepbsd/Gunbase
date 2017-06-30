5e5a9a2c1d80f6501255c60901a0af976fe1e235
Matthew Day <matt@matthewday.net>
2017-06-30 15:48:44 -0400

    Update db path for this branch

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
-                      'mongodb://localhost/firearms';
-exports.TEST_DATABASE_URL = (
-   process.env.TEST_DATABASE_URL ||
-   'mongodb://localhost/testfirearms');
+                      'mongodb://localhost/djfire';
+// exports.TEST_DATABASE_URL = (
+//    process.env.TEST_DATABASE_URL ||
+//    'mongodb://localhost/testfirearms');
 
 exports.PORT = process.env.PORT || 8080;
