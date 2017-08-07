5fa8f86d9e0a46e2ac76fc5140d0de5861ee0731
David S. Jackson <deepbsd@yahoo.com>
2017-08-07 16:24:50 -0400

    added deleteGun and getOneGun to refactored code

--- src/js/main-matt.js
+++ src/js/main-matt.js
@@ -36,6 +36,29 @@
         app.postNewGun(gunObj);
       })
     },
+    deleteGun: function(gunId){
+      var myURL = app.rootURL + '/' + gunId;
+      $.ajax({
+        url: myURL,
+        type: 'DELETE',
+        headers: {
+          "accept": "application/json;odata=verbose",
+        },
+
+        contentType: "application/json; charset=utf-8",
+        dataType: 'json',
+        success: function() {
+
+          console.log(`Gun ${gunId} deleted!`);
+          app.getAllGuns()
+            .then(app.outputGunsReport);
+
+        },
+        error: function(error){
+          console.log('Update failed.  Error: ',error);
+        }
+      })
+    },
     filterSearch: function() {
       $(document).on('input', "#top-form-find", function(){
         let criteria = $("#top-find").val();
@@ -83,7 +106,89 @@
       }
     },
     getOneGun: function(gunId) {
-      alert(gunId);
+      var myURL = app.rootURL + '/' + gunId;
+      $.ajax({
+        url: myURL,
+        type: 'GET',
+        headers: {
+          "accept": "application/json;odata=verbose",
+        },
+        contentType: "application/json; charset=utf-8",
+        dataType: 'json',
+        success: function(gundata) {
+
+          console.log('HEY!  Object: ',gundata);
+          var template = '<div id="update_gun_function"><form id="update_gun_form">';
+          template += `<img class="medium-img"  src="${gundata.image}"/>`;
+          template += `<div class="fieldWrapWrapper"><div class="basicFieldWrap">`
+          template += `<label for="manufacturer">Manufacturer</label>`;
+          template += `<input id="manufacturer" type="text" placeholder="${gundata.manufacturer}" name="manufacturer" value="${gundata.manufacturer}">`;
+          template += `<label for="model">Model</label>`;
+          template += `<input id="model"  type="text" placeholder="${gundata.model}" name="model" value="${gundata.model}">`;
+          template += `<label for="chambering">Chambering</label>`;
+          template += `<input id="chambering"  type="text" placeholder="${gundata.chambering}" name="chambering" value="${gundata.chambering}">`;
+          template += `<label for="type">Type</label>`;
+          template += `<input id="type" type="text" placeholder="${gundata.type}" name="type" value="${gundata.type}">`;
+          template += `<label for="serial_number">Serial Number</label>`;
+          template += `<input  id="serial_number" type="text" placeholder="${gundata.serial_number}" name="serial_number" value="${gundata.serial_number}">`;
+          template += `</div><div class="basicFieldWrap">`
+          template += `<label for="image">Image</label>`;
+          template += `<input id="image" type="text" placeholder="${app.imgPath(gundata.image)}" name="image" value="${app.imgPath(gundata.image)}">`;
+          template += `<label for="value">Dollar Value</label>`;
+          template += `<input id="value" type="text" placeholder="${gundata.value}" name="value" value="${gundata.value}">`;
+          template += `<label for="sold">Sold</label>`;
+          template += `<input id="sold" type="text" placeholder="${gundata.sold}" name="sold" value="${gundata.sold}">`;
+          template += `<label for="buyer">Buyer</label>`;
+          template += `<input id="buyer" type="text" placeholder="${gundata.buyer}" name="buyer" value="${gundata.buyer}">`;
+          template += '<div class="btn_wrapper"><button type="submit" id="update_gun_submit">Update</submit> ';
+          template += '<button type="submit" id="delete_gun_submit">Delete</div></submit>';
+          template += `</div></div>`;  //end of second basicFieldWrap and fieldWrapWrapper
+          template += '</form></div>';
+          $("#output").html(template);
+
+
+          // Update_gun listener
+          $("#update_gun_submit").click(function(ev){
+            ev.preventDefault();
+            let fields = ['manufacturer', 'model', 'chambering', 'type', 'serial_number', 'image', 'value', 'sold', 'buyer'];
+
+            let updateData = {};
+            updateData.id = `${gunId}`;
+
+            //Modify this so you can update more than one field!!!
+            fields.forEach(function(field){
+              if ($('#'+field).val()) {
+                let thevalue = $('#'+field).val();
+                if (field == 'image'){
+                  updateData[field] = `img/${thevalue}`;
+                } else {
+                  updateData[field] = `${thevalue}`;
+                }
+              }
+            })
+
+            console.log('TYPEOF:  ',typeof updateData, ' DATA: ',updateData);
+            updateGun(updateData, gunId);
+          })
+
+          //If the delete button gets pressed...
+          // ###################################
+          $("#delete_gun_submit").click(function(ev){
+            ev.preventDefault();
+            console.log('Deleting gun with id: ', gunId);
+
+            app.deleteGun(gunId);
+            app.outputGunsReport();
+          })
+        },
+        error: function(error){
+          console.log('error *getting* the record: ',error);
+        }
+      })
+
+    },
+    imgPath: function(obj){
+      return obj.substring(obj.lastIndexOf("/")+1, obj.length);
     },
     init: function() {
       app.getAllGuns().then(app.outputGunsReport);
